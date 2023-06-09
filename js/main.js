57a609c4722371f79a035a89b476d2425fa2adad
Erick Omondi <51463912+ErickOmondi97@users.noreply.github.com>
2023-06-09 21:20:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var tablinks = document.getElementsByClassName("tab-links");
-  var tabcontents = document.getElementsByClassName("tab-contents");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
   function opentab(tabname){
     for(tablink of tablinks){
@@ -12,3 +12,44 @@ var tablinks = document.getElementsByClassName("tab-links");
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
   }
+
+
+    //ALT FOR MENU BUTTON
+  /* var sidemenu = document.getElementById("sidemenu");
+
+  function openmenu(){
+    sidemenu.style.right = "0";
+  }
+  function closemenu(){
+    sidemenu.style.right = "255";
+  }
+ */
+
+//MENU BUTTON
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if(!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach(item => item.classList.add('open'));
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach(item => item.classList.remove('open'));
+
+    showMenu = false;
+  }
+}
